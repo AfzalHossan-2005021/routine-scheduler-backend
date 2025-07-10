6dc8b8eded211ae8897e2f40d3770aa1bb9d20bb
AfzalHossan-2005021 <afzalhossan2005021@gmail.com>
2025-07-10 22:24:12 +0600

    section count for active batch coordination added.

--- src/information/level_terms/route.js
+++ src/information/level_terms/route.js
@@ -2,10 +2,12 @@ import express from 'express'
 
 const router = express.Router();
 
-import { getAllLevelTerms, setLevelTerms, getAllActiveDepartmentsAPI, getDepartmentalLevelTermBatchesAPI } from './controller.js';
+import { getAllLevelTerms, setLevelTerms, addLevelTermAPI, deleteLevelTermAPI, getAllActiveDepartmentsAPI, getDepartmentalLevelTermBatchesAPI } from './controller.js';
 
 router.get("/", getAllLevelTerms);
 router.put("/set", setLevelTerms);
+router.post("/add", addLevelTermAPI);
+router.delete("/delete", deleteLevelTermAPI);
 router.get("/active-departments", getAllActiveDepartmentsAPI);
 router.get("/departmental_level_term_batches/:department", getDepartmentalLevelTermBatchesAPI);
 
