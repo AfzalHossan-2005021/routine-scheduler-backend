eaef4d62dfb32acfe7d910e14e481b810d15cccc
AfzalHossan-2005021 <afzalhossan2005021@gmail.com>
2025-07-16 21:02:57 +0600

    Online work added.

--- src/online/route.js
+++ src/online/route.js
@@ -2,12 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
-import { getAllTeacher, getTeacher, addTeacher, editTeacher, deleteTeacher } from "./controller.js";
+import { getAllTeacher } from "./controller.js";
 
 router.get("/get/all", getAllTeacher);
-router.get("/get/:initial", getTeacher);
-router.post("/add", addTeacher);
-router.put("/edit/:initial", editTeacher);
-router.delete("/delete/:initial", deleteTeacher);
 
 export default router;
